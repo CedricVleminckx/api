68b40fbf007576f0b2f4ac8b12cbc97c0eec3a0c
Cedric <Cedric@DESKTOP-BTF3HDL>
2018-07-25 10:39:55 +0200

    removed inviter

--- resources/views/emails/invite-picture.blade.php
+++ resources/views/emails/invite-picture.blade.php
@@ -13,7 +13,6 @@
 <p>
    {{ $message }}
 </p>
-<!--<p><a href="">Antwoord op {{ $inviter }}</a></p>-->
 <p><a href="https://app.prisma.care/login">Bekijk jouw verhalen</a></p>
 
 @if ($token != 'NoToken')
